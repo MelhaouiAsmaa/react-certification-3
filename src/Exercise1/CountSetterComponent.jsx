50c19e4e869def700da8a90e4133fb9e8a662938
Asmaa Melhaoui <melhaouiasmaa@gmail.com>
2025-04-24 13:29:31 +0100

    ex1 update

--- src/Exercise1/CountSetterComponent.jsx
+++ src/Exercise1/CountSetterComponent.jsx
@@ -1,25 +1,30 @@
 import { CountSubscriberComponent } from "./CountSubscriberComponent";
-import  usePersistedState  from "./usePersistedState";
+import usePersistedState from "./usePersistedState";
 
 export function CountSetterComponent() {
   const [count, setCount] = usePersistedState("count", 0);
 
   function handleIncrement() {
-    setCount(count => count + 1);
+    setCount((count) => count + 1);
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className=" p-6 shadow-lg max-w-md text-center">
-        <h2 className="text-2xl font-bold mb-4">Component 1</h2>
-        <button
-          className="btn btn-primary text-white font-bold py-2 px-6 rounded mb-4"
-          onClick={handleIncrement}
-        >
-          Increment
-        </button>
-        <CountSubscriberComponent key={count} subscribedValue="count" />
-      </div>
+    <div className="row">
+        {/* Left side: Title + Button */}
+        <div className="col-6 text-center bg-primary">
+          <h2 className="text-2xl font-bold mb-4">Component 1</h2>
+          <button
+            className="btn btn-primary text-white font-bold py-2 px-6 rounded"
+            onClick={handleIncrement}
+          >
+            Increment
+          </button>
+          <p>{count}</p>
+        </div>
+  
+        {/* Right side: CountSubscriberComponent */}
+        <CountSubscriberComponent className="col-6 text-center" subscribedValue="count" />
     </div>
   );
+  
 }
--- src/Exercise1/CountSubscriberComponent.jsx
+++ src/Exercise1/CountSubscriberComponent.jsx
@@ -1,12 +1,12 @@
 import { subscribeToState } from "./usePersistedState"
 
-export function CountSubscriberComponent({subscribedValue}) {
-    const count = subscribeToState(subscribedValue);
-        
-    return (
-        <div className="bg-blue-100 rounded-2xl p-6 shadow-lg max-w-md w-full text-center mx-auto mt-6">
-          <h2 className="text-2xl font-bold mb-4">Component 2</h2>
-          <h3 className="text-3xl font-semibold">{count}</h3>
-        </div>
-      );
+export function CountSubscriberComponent({className, subscribedValue }) {
+  const count = subscribeToState(subscribedValue);
+
+  return (
+    <div className={className}>
+      <h2 className="text-2xl font-bold mb-4">Component 2</h2>
+      <h3 className="text-3xl font-semibold">{count}</h3>
+    </div>
+  );
 }
